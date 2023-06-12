7167465a75f2906e2a0059557fb470fdb39f02f0
LeandeR <wi22b088@technikum-wien.at>
2023-06-13 01:42:08 +0200

    DateTimeTest added

---
+++ src/test/java/at/technikum/slmabschlusstest/DateTimeTest.java
@@ -0,0 +1,22 @@
+package at.technikum.slmabschlusstest;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DateTimeTest {
+
+    @Test
+    void getDate() {
+        DateTime dateTime = new DateTime();
+        assertEquals(LocalDate.now(),dateTime.getDate());
+    }
+
+    void getTime() {
+        DateTime dateTime = new DateTime();
+        assertEquals(LocalTime.now(),dateTime.getTime());
+    }
+
+}
